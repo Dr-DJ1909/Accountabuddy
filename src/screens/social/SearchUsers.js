169f43de0b827a4bbe95694516c0c24dcc3a72f8
Lin Xia <linyianxia@hotmail.com>
2019-12-01 15:16:32 -0500

    onPress aka not a real button, does successfully add-friend

--- src/screens/social/SearchUsers.js
+++ src/screens/social/SearchUsers.js
@@ -6,7 +6,8 @@ import {
   View,
   FlatList,
   StyleSheet,
-  AsyncStorage
+  AsyncStorage,
+  Button
 } from 'react-native';
 import {
   PageWrapperView,
@@ -110,6 +111,10 @@ class SearchUsers extends React.Component {
                 // leftAvatar={{source: {uri: item.picture.thumbnail}}}
                 title={item.email}
                 subtitle={item.UserName}
+                onPress={() => {
+                  newFriend(this.state.userKey, item.uId);
+                  newFriend(item.uId, this.state.userKey);
+                }}
               />
             )}
             // keyExtractor={item => item.email}
@@ -119,21 +124,7 @@ class SearchUsers extends React.Component {
           />
         </View>
       );
-    }
-    // return (
-    //   <SafeAreaView>
-    //     <TasksHeader></TasksHeader>
-    //     <FlatList
-    //       data={users}
-    //       keyExtractor={(item, index) => index.toString()}
-    //       renderItem={({item}) => (
-    //         <ListUsers item={item} uId={this.state.userKey} />
-    //       )}
-    //     ></FlatList>
-    //   </SafeAreaView>
-    // );
-    // }
-    else {
+    } else {
       return <View></View>;
     }
   }
@@ -155,103 +146,3 @@ const styles = StyleSheet.create({
     fontSize: 32
   }
 });
-
-// import React, {Component} from 'react';
-// import {View, Text, FlatList, ActivityIndicator} from 'react-native';
-// import {ListItem, SearchBar} from 'react-native-elements';
-// import {getUsers} from '../../api/UserRoute';
-// class SearchUsers extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       loading: false,
-//       data: [],
-//       error: null
-//     };
-//     this.arrayholder = [];
-//   }
-//   async componentDidMount() {
-//     this.makeRemoteRequest();
-//   }
-//   makeRemoteRequest = () => {
-//     const url = `https://randomuser.me/api/?&results=20`;
-//     this.setState({loading: true});
-//     fetch(url)
-//       .then(res => res.json())
-//       .then(res => {
-//         this.setState({
-//           data: res.results,
-//           error: res.error || null,
-//           loading: false
-//         });
-//         this.arrayholder = res.results;
-//       })
-//       .catch(error => {
-//         this.setState({error, loading: false});
-//       });
-//   };
-//   renderSeparator = () => {
-//     return (
-//       <View
-//         style={{
-//           height: 1,
-//           width: '86%',
-//           backgroundColor: '#CED0CE',
-//           marginLeft: '14%'
-//         }}
-//       />
-//     );
-//   };
-//   searchFilterFunction = text => {
-//     this.setState({
-//       value: text
-//     });
-//     const newData = this.arrayholder.filter(item => {
-//       const itemData = `${item.name.title.toUpperCase()} ${item.name.first.toUpperCase()} ${item.name.last.toUpperCase()}`;
-//       const textData = text.toUpperCase();
-//       return itemData.indexOf(textData) > -1;
-//     });
-//     this.setState({
-//       data: newData
-//     });
-//   };
-//   renderHeader = () => {
-//     return (
-//       <SearchBar
-//         placeholder="Search friend name..."
-//         lightTheme
-//         round
-//         onChangeText={text => this.searchFilterFunction(text)}
-//         autoCorrect={false}
-//         value={this.state.value}
-//       />
-//     );
-//   };
-//   render() {
-//     if (this.state.loading) {
-//       return (
-//         <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-//           <ActivityIndicator />
-//         </View>
-//       );
-//     }
-//     return (
-//       <View style={{flex: 1}}>
-//         <FlatList
-//           data={this.state.data}
-//           renderItem={({item}) => (
-//             <ListItem
-//               leftAvatar={{source: {uri: item.picture.thumbnail}}}
-//               title={`${item.name.first} ${item.name.last}`}
-//               subtitle={item.email}
-//             />
-//           )}
-//           keyExtractor={item => item.email}
-//           ItemSeparatorComponent={this.renderSeparator}
-//           ListHeaderComponent={this.renderHeader}
-//         />
-//       </View>
-//     );
-//   }
-// }
-// export default SearchUsers;
