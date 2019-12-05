1ac7be27fee811f66be43d9bf0f78ef156582f4d
gjyhuang <grace.jy.huang@gmail.com>
2019-12-05 18:30:46 -0500

    [fix] login/logout

--- App.js
+++ App.js
@@ -13,15 +13,31 @@ import NavWrapper from './src/components/NavWrapper';
 import PersistedLogin from './src/components/PersistedLogin';
 import ignoreWarnings from 'react-native-ignore-warnings';
 import {getUser} from './src/api/UserRoute';
-import Auth from './Auth'
 // import { createStackNavigator } from 'react-navigation-stack'
 
 ignoreWarnings('Setting a timer');
 ignoreWarnings('Require cycle');
 
+const MainNavigator = createSwitchNavigator(
+  {
+    SignUpLogIn: {screen: SignUpLogIn},
+    TestPetScreen: {screen: TestPetScreen},
+    UserNameScreen: {screen:UserNameScreen},
+    NavWrapper: {screen: NavWrapper}
+  },
+  {
+    backBehavior: 'none'
+  }
+);
+
+const AppLogin = createAppContainer(MainNavigator);
+
 export default class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      loading: true,
+    };
   }
 
   componentWillMount() {
@@ -29,12 +45,29 @@ export default class App extends Component {
     firebase.initializeApp(config);
   }
 
-    render() {
+  componentDidMount() {
+    this.authSubscription = firebase.auth().onAuthStateChanged((user) => {
+      this.setState({
+        loading: false,
+        user,
+      });
+    });
+  }
+
+  render() {
+    if (this.state.loading) return null;
+    if (this.state.user) {
+      getUser(this.state.user.uid);
       return (
         <Provider store={Store}>
-
-          <Auth />
+          <PersistedLogin userKey={this.state.user.uid}/>
         </Provider>
       )
     }
+    return (
+      <Provider store={Store}>
+        <AppLogin />
+      </Provider>
+    );
+  }
 }
